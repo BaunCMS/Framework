eac6ee861cf2e374b4e9758378633992e6abe7f9
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-05 10:40:39 +0000

    Theme render fix

--- src/Providers/Theme.php
+++ src/Providers/Theme.php
@@ -15,7 +15,7 @@ class Theme implements ThemeInterface {
 
 	public function render($template, $data = [])
 	{
-		return $this->theme->render($template . '.html', $data);
+		echo $this->theme->render($template . '.html', $data);
 	}
 
 	public function custom($name, $data)
